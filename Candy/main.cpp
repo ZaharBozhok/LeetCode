2ce80189d0537cee1a65b40bea5d24394dc656f0
Bozhok Zakhar <3aharbozhok@gmail.com>
2020-03-15 22:41:01 +0200

    refactoring

--- Candy/main.cpp
+++ Candy/main.cpp
@@ -12,21 +12,18 @@ enum Direction
 enum Extremum
 {
     Min,
-    Max
+    Max,
+    None
 };
 
 struct Data
 {
-    Data(Extremum ext, int value, int index) 
-    : left(nullptr)
-    , right(nullptr)
-    , set(false)
-    , ext(ext)
-    , value(value)
-    , index(index)
-    {}
-    Data* left;
-    Data* right;
+    Data(Extremum ext, int value, int index)
+        : left(nullptr), right(nullptr), set(false), ext(ext), value(value), index(index)
+    {
+    }
+    Data *left;
+    Data *right;
     bool set;
     Extremum ext;
     int value;
@@ -42,33 +39,34 @@ Direction GetDirection(int first, int second)
     return Direction::Falling;
 }
 
-void show(const std::vector<int> &vec, std::vector<Data> extremums, const std::vector<int>& eliminates)
+void show(const std::vector<int> &vec, std::vector<Data> extremums, const std::vector<int> &eliminates)
 {
     auto maxRate = *std::max_element(vec.cbegin(), vec.cend());
-    std::vector<std::vector<char>> board(maxRate+1, std::vector<char>(vec.size(), '-'));
-    for(int coli=0; coli<vec.size(); coli++)
+    std::vector<std::vector<char>> board(maxRate + 1, std::vector<char>(vec.size(), '-'));
+    for (int coli = 0; coli < vec.size(); coli++)
     {
-        for(int rowi = 0; rowi < vec.at(coli) + 1; rowi++)
+        for (int rowi = 0; rowi < vec.at(coli) + 1; rowi++)
         {
             board[rowi][coli] = 'O';
         }
     }
-    for(auto it = board.crbegin(); it != board.crend(); it++)
+    for (auto it = board.crbegin(); it != board.crend(); it++)
     {
-        std::cout <<  maxRate--;
-        for(auto jt = it->cbegin(); jt != it->cend(); jt++)
+        std::cout << maxRate--;
+        for (auto jt = it->cbegin(); jt != it->cend(); jt++)
         {
-            std::cout << "--"<< *jt;
+            std::cout << "--" << *jt;
         }
         std::cout << std::endl;
     }
     std::cout << ' ';
-    for (int i=0; i<vec.size(); i++)
+    for (int i = 0; i < vec.size(); i++)
     {
         std::cout << std::setw(3) << i;
     }
-    std::cout << std::endl << ' ';
-    for(int i=0, exi=0; i<vec.size(); i++)
+    std::cout << std::endl
+              << ' ';
+    for (int i = 0, exi = 0; i < vec.size(); i++)
     {
         char symbol = ' ';
         if (i == extremums[exi].index)
@@ -81,8 +79,9 @@ void show(const std::vector<int> &vec, std::vector<Data> extremums, const std::v
         }
         std::cout << std::setw(3) << symbol;
     }
-    std::cout << std::endl << ' ';
-    for(int i=0, xi=0; i<vec.size() && xi <  eliminates.size() ; i++)
+    std::cout << std::endl
+              << ' ';
+    for (int i = 0, xi = 0; i < vec.size() && xi < eliminates.size(); i++)
     {
         char symbol = ' ';
         if (i == eliminates.at(xi))
@@ -95,8 +94,22 @@ void show(const std::vector<int> &vec, std::vector<Data> extremums, const std::v
     std::cout << std::endl;
 }
 
+Extremum getChange(const Direction &last, const Direction &curr)
+{
+    if (((last == Direction::Falling || last == Direction::Plain) && curr == Direction::Rising) ||
+        (last == Direction::Falling && curr == Direction::Plain))
+    {
+        return Extremum::Min;
+    }
+    if (((last == Direction::Rising || last == Direction::Plain) && curr == Direction::Falling) ||
+        (last == Direction::Rising && curr == Direction::Plain))
+    {
+        return Extremum::Max;
+    }
+    return Extremum::None;
+}
 
-void GetExtremums(std::vector<Data> &extremums, std::vector<int>& eliminates, const std::vector<int>& elements)
+void GetExtremums(std::vector<Data> &extremums, std::vector<int> &eliminates, const std::vector<int> &elements)
 {
     if (elements.empty())
         return;
@@ -107,32 +120,42 @@ void GetExtremums(std::vector<Data> &extremums, std::vector<int>& eliminates, co
     }
     Direction lastDirection = GetDirection(elements.at(0), elements.at(1));
     if (lastDirection == Direction::Falling)
+    {
         extremums.push_back(Data(Extremum::Max, elements[0], 0));
+    }
     else if (lastDirection == Direction::Rising)
+    {
         extremums.push_back(Data(Extremum::Min, elements[0], 0));
+    }
     for (int i = 1; i < elements.size() - 1; i++)
     {
         auto direction = GetDirection(elements.at(i), elements.at(i + 1));
-        if (((lastDirection == Direction::Falling || lastDirection == Direction::Plain) && direction == Direction::Rising) ||
-        (lastDirection == Direction::Falling && direction == Direction::Plain))
+        switch (getChange(lastDirection, direction))
         {
+        case Extremum::Min:
             extremums.push_back(Data(Extremum::Min, elements[i], i));
-        } 
-        else if (((lastDirection == Direction::Rising || lastDirection == Direction::Plain) && direction == Direction::Falling) ||
-        (lastDirection == Direction::Rising && direction == Direction::Plain))
-        {
+            break;
+        case Extremum::Max:
             extremums.push_back(Data(Extremum::Max, elements[i], i));
+            break;
+        case Extremum::None:
+            if (direction == Direction::Plain)
+            {
+                eliminates.push_back(i);
+            }
+            break;
         }
-        else if (direction == Direction::Plain)
-        {
-            eliminates.push_back(i);
-        }
+
         lastDirection = direction;
     }
     if (lastDirection == Direction::Falling)
+    {
         extremums.push_back(Data(Extremum::Min, elements[elements.size() - 1], elements.size() - 1));
+    }
     else if (lastDirection == Direction::Rising)
+    {
         extremums.push_back(Data(Extremum::Max, elements[elements.size() - 1], elements.size() - 1));
+    }
 }
 
 int GetNthProgressionSum(int n)
@@ -140,14 +163,13 @@ int GetNthProgressionSum(int n)
     return (n + 1) * (n / 2.0);
 }
 
-
 int main()
 {
     std::vector<int> ratings = {5, 4, 6, 2, 2, 6, 7, 5, 6, 5, 6, 7, 7, 7, 6, 5, 4, 4, 4, 3, 6, 4, 2, 1, 4, 5, 6, 7, 0, 6};
     std::vector<int> eliminates;
     std::vector<Data> extremums;
     GetExtremums(extremums, eliminates, ratings);
-    std::cout << std::endl;
     show(ratings, extremums, eliminates);
+    std::cout << std::endl;
     return 0;
 }
