251e6595abda002f0dfa28346ecafcb0cfad34ea
Bozhok Zakhar <3aharbozhok@gmail.com>
2020-03-21 19:18:59 +0200

    added squeeze through

--- IntervalMap/IntervalMap.h
+++ IntervalMap/IntervalMap.h
@@ -1,27 +1,33 @@
 #ifndef INTERVALMAP_H
 #define INTERVALMAP_H
 
+#include <iterator>
 #include <limits>
 #include <map>
-#include <iterator>
 
-template <class K, class V> class IntervalMap {
+template <class K, class V>
+class IntervalMap
+{
 public:
-  IntervalMap(V const &val) {
+  using kType = K;
+  using vType = V;
+
+public:
+  IntervalMap(V const &val)
+  {
     m_map.insert(m_map.begin(),
                  std::make_pair(std::numeric_limits<K>::min(), val));
   }
-  V &operator[](K const &key) {
+  const V &operator[](K const &key) const
+  {
     auto it = m_map.upper_bound(key);
     --it;
     return it->second;
   }
 
-  void assign(K const &keyBegin, K const &keyEnd, V const &val) {
-	  
-  }
+  void assign(K const &keyBegin, K const &keyEnd, V const &val) {}
 
 private:
   std::map<K, V> m_map;
 };
-#endif //INTERVALMAP_H
+#endif // INTERVALMAP_H
--- IntervalMap/test.cpp
+++ IntervalMap/test.cpp
@@ -1,16 +1,55 @@
+#include "IntervalMap.h"
 #include <gtest/gtest.h>
 #include <limits>
-#include "IntervalMap.h"
 
 using K = uint8_t;
 
+template <class TMap>
+bool RangeEqualsTo(const TMap &map,
+                   const typename TMap::kType &begin,
+                   const typename TMap::kType &end,
+                   const typename TMap::vType &val)
+{
+    for (auto i = begin; i < end; i++)
+    {
+        if (map[i] != val)
+            return false;
+    }
+    return true;
+}
+
 TEST(IntervalMap, FilledWithInitialValue)
 {
-    char value = 'A';
-    IntervalMap<K, decltype(value)> map(value);
+    IntervalMap<K, char> map('A');
+    ASSERT_TRUE(
+        RangeEqualsTo(map,
+                      std::numeric_limits<K>::min(),
+                      std::numeric_limits<K>::max(),
+                      'A'));
+}
 
-    for(auto i=std::numeric_limits<K>::min(); i<std::numeric_limits<K>::max(); i++)
-    {
-        ASSERT_EQ(map[i], value);
-    }
+TEST(IntervalMap, SqueezeThrough)
+{
+    char initialVal = 'A';
+    IntervalMap<K, decltype(initialVal)> map(initialVal);
+
+    const K insertBegin = 10, insertEnd = 20;
+    const decltype(initialVal) insertVal = 'B';
+    map.assign(insertBegin, insertEnd, insertVal);
+
+    ASSERT_TRUE(
+        RangeEqualsTo(map,
+                      std::numeric_limits<K>::min(),
+                      insertBegin,
+                      initialVal));
+    ASSERT_TRUE(
+        RangeEqualsTo(map,
+                      insertBegin,
+                      insertEnd,
+                      insertVal));
+    ASSERT_TRUE(
+        RangeEqualsTo(map,
+                      insertEnd,
+                      std::numeric_limits<K>::max(),
+                      initialVal));
 }
