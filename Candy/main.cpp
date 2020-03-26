8c35ae895ae8dfd402c30c51768115531e5c7ef1
Bozhok Zakhar <3aharbozhok@gmail.com>
2020-03-26 10:58:00 +0200

    morreee difficults

--- Candy/main.cpp
+++ Candy/main.cpp
@@ -1,6 +1,8 @@
-#include <vector>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <list>
+#include <unordered_map>
+#include <vector>
 
 enum Direction
 {
@@ -66,7 +68,7 @@ void show(const std::vector<int> &vec, std::vector<Data> extremums, const std::v
     }
     std::cout << std::endl
               << ' ';
-    for (int i = 0, exi = 0; i < vec.size(); i++)
+    for (int i = 0, exi = 0; i < vec.size() && exi < extremums.size(); i++)
     {
         char symbol = ' ';
         if (i == extremums[exi].index)
@@ -111,36 +113,73 @@ Extremum getChange(const Direction &last, const Direction &curr)
 
 void GetExtremums(std::vector<Data> &extremums, std::vector<int> &eliminates, const std::vector<int> &elements)
 {
+    std::unordered_map<int, std::vector<Data *>> risingMap;
     if (elements.empty())
         return;
     if (elements.size() == 1)
     {
-        extremums.push_back(Data(Extremum::Min, elements[0], 0));
+        eliminates.push_back(0);
         return;
     }
+    Data *lastMax = nullptr;
+    Data *lastMin = nullptr;
     Direction lastDirection = GetDirection(elements.at(0), elements.at(1));
     if (lastDirection == Direction::Falling)
     {
         extremums.push_back(Data(Extremum::Max, elements[0], 0));
+        lastMax = new Data(Extremum::Max, elements[0], 0);
     }
     else if (lastDirection == Direction::Rising)
     {
         extremums.push_back(Data(Extremum::Min, elements[0], 0));
+        lastMin = new Data(Extremum::Min, elements[0], 0);
+        auto it = risingMap.find(elements[0]);
+        if (it != risingMap.cend())
+        {
+            it->second.push_back(lastMin);
+        }
+        else
+        {
+            risingMap.insert({elements[0], std::vector{lastMin}});
+        }
+    }
+    else if (lastDirection == Direction::Plain)
+    {
+        eliminates.push_back(0);
     }
     for (int i = 1; i < elements.size() - 1; i++)
     {
         auto direction = GetDirection(elements.at(i), elements.at(i + 1));
         switch (getChange(lastDirection, direction))
         {
-        case Extremum::Min:
-            extremums.push_back(Data(Extremum::Min, elements[i], i));
+            case Extremum::Min:
+            {
+                extremums.push_back(Data(Extremum::Min, elements[i], i));
+                lastMin = new Data(Extremum::Min, elements[i], i);
+                if (lastMax)
+                    lastMin->left = lastMax;
+                auto it = risingMap.find(elements[i]);
+                if (it != risingMap.cend())
+                {
+                    it->second.push_back(lastMin);
+                }
+                else
+                {
+                    risingMap.insert({elements[i], std::vector{lastMin}});
+                }
+            }
             break;
         case Extremum::Max:
             extremums.push_back(Data(Extremum::Max, elements[i], i));
+            lastMax = new Data(Extremum::Max, elements[i], i);
+            if (lastMin)
+                lastMin->right = lastMax;
             break;
         case Extremum::None:
             if (direction == Direction::Plain)
             {
+                lastMin = nullptr;
+                lastMax = nullptr;
                 eliminates.push_back(i);
             }
             break;
@@ -151,10 +190,29 @@ void GetExtremums(std::vector<Data> &extremums, std::vector<int> &eliminates, co
     if (lastDirection == Direction::Falling)
     {
         extremums.push_back(Data(Extremum::Min, elements[elements.size() - 1], elements.size() - 1));
+        lastMin = new Data(Extremum::Min, elements[elements.size() - 1], elements.size() - 1);
+        if (lastMax)
+            lastMin->left = lastMax;
+        auto it = risingMap.find(elements[elements.size() - 1]);
+        if (it != risingMap.cend())
+        {
+            it->second.push_back(lastMin);
+        }
+        else
+        {
+            risingMap.insert({elements[elements.size() - 1], std::vector{lastMin}});
+        }
     }
     else if (lastDirection == Direction::Rising)
     {
         extremums.push_back(Data(Extremum::Max, elements[elements.size() - 1], elements.size() - 1));
+        lastMax = new Data(Extremum::Max, elements[elements.size() - 1], elements.size() - 1);
+        if (lastMin)
+            lastMin->right = lastMax;
+    }
+    else if (lastDirection == Direction::Plain)
+    {
+        eliminates.push_back(elements.size() - 1);
     }
 }
 
@@ -165,7 +223,8 @@ int GetNthProgressionSum(int n)
 
 int main()
 {
-    std::vector<int> ratings = {5, 4, 6, 2, 2, 6, 7, 5, 6, 5, 6, 7, 7, 7, 6, 5, 4, 4, 4, 3, 6, 4, 2, 1, 4, 5, 6, 7, 0, 6};
+    std::vector<int> ratings = {5, 5, 4, 6, 2, 2, 6, 7, 5, 6, 5, 6, 7, 7, 7, 6, 5, 4, 4, 4, 3, 6, 4, 2, 1, 4, 5, 6, 7, 0, 6, 6};
+    //std::vector<int> ratings = {5};
     std::vector<int> eliminates;
     std::vector<Data> extremums;
     GetExtremums(extremums, eliminates, ratings);
